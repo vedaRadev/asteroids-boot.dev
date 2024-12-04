24049a89efb7bbe84adcdff98b505da4f900d260
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-04 17:08:28 -0500

    add constants, update gitignore

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+from constants import *
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+# Only run the following if this file was run directly.
+# Avoids running when we're imported as a module.
+if __name__ == "__main__":
+    main()
