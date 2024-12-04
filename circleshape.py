66cf3a438f2cd900f1d08ed94bc3b867f8d31793
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-04 18:26:22 -0500

    draw player to screen

---
+++ circleshape.py
@@ -0,0 +1,20 @@
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+        # subclasses override
+        def draw(self, screen):
+            pass
+
+        # subclasses override
+        def update(self, dt):
+            pass
--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+import os
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
+    os.environ["SDL_VIDEODRIVER"] = "x11"
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    _ = pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        _ = screen.fill(pygame.Color(0, 0, 0))
+        player.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 
 # Only run the following if this file was run directly.
