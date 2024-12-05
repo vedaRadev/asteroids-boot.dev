25f8c0f9450cc470ad8bad705ebf361b4a08ff12
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-04 19:38:11 -0500

    rate limit shooting

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOT_SPEED
+import constants
 from circleshape import CircleShape
 from shot import Shot
 
@@ -7,14 +7,21 @@ class Player(CircleShape):
     containers = None
 
     def __init__(self, x, y):
-        super().__init__(x, y, PLAYER_RADIUS)
+        super().__init__(x, y, constants.PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
 
     def draw(self, screen):
         pygame.draw.polygon(screen, pygame.Color(255, 255, 255), self.triangle(), 2)
 
+
     def update(self, dt):
+        if self.shot_timer > 0:
+            self.shot_timer -= dt
+            if self.shot_timer < 0:
+                self.shot_timer = 0
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -25,27 +32,29 @@ class Player(CircleShape):
 
         if keys[pygame.K_w]:
             self.move(dt)
-        
+
         if keys[pygame.K_s]:
             self.move(-dt)
 
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shot_timer == 0:
+                self.shoot()
 
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        self.shot_timer = constants.PLAYER_SHOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y, constants.SHOT_RADIUS)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
-        shot.velocity *= PLAYER_SHOT_SPEED
+        shot.velocity *= constants.PLAYER_SHOT_SPEED
 
 
     def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
+        self.rotation += constants.PLAYER_TURN_SPEED * dt
 
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+        self.position += forward * constants.PLAYER_SPEED * dt
 
 
     def triangle(self):
